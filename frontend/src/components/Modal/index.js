c245bfc692ba7398429b508e258b9bb439fa39ec
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-26 08:29:38 -0300

    feat: changing reactPortal to a separate component

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
 
 import { Overlay, Container, Footer } from './styles';
 
 import Button from '../Button';
+import ReactPortal from '../ReactPortal';
 
 export default function Modal({
   danger,
@@ -18,25 +18,26 @@ export default function Modal({
 }) {
   if (!visible) return null;
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
 
-        <div className="modal-body">{children}</div>
+          <div className="modal-body">{children}</div>
 
-        <Footer>
-          <button type="button" className="CancelButton" onClick={onCancel} disabled={isLoading}>
-            {cancelLabel}
-          </button>
-          <Button type="button" danger={danger} onClick={onConfirm} isLoading={isLoading}>
-            {confirmLabel}
-          </Button>
-        </Footer>
+          <Footer>
+            <button type="button" className="CancelButton" onClick={onCancel} disabled={isLoading}>
+              {cancelLabel}
+            </button>
+            <Button type="button" danger={danger} onClick={onConfirm} isLoading={isLoading}>
+              {confirmLabel}
+            </Button>
+          </Footer>
 
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
