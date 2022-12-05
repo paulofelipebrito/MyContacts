8361d93d519a3d3287d8008a10bee0d283ad854b
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-12-05 20:05:53 -0300

    feat: change import to export default

--- frontend/src/pages/EditContact/index.js
+++ frontend/src/pages/EditContact/index.js
@@ -1,6 +1,2 @@
-import Container from './Container';
-import Presentation from './Presentation';
-
-export {
-  Container, Presentation,
-};
+export { default as Container } from './Container';
+export { default as Presentation } from './Presentation';
