9b7e0abdba6b92db6f15656b313984e27194aac5
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-12-05 20:04:37 -0300

    feat: add Container/Presentation pattern

---
+++ frontend/src/pages/EditContact/Container.js
@@ -0,0 +1,67 @@
+import { useHistory, useParams } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+import Presentation from './Presentation';
+
+export default function Container() {
+  const [contactName, setContactName] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+  const safeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        safeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setIsLoading(false);
+          setContactName(contact.name);
+        });
+      } catch {
+        safeAsyncAction(() => {
+          history.push('./');
+          toast({
+            type: 'danger',
+            text: 'Contact não encontrado!',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, safeAsyncAction]);
+
+  async function handleSubmit(contact) {
+    try {
+      const updatedContactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(updatedContactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
+  return (
+    <Presentation
+      isLoading={isLoading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
