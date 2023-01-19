b44b0c0df29230b0fbd96c7637fd254e1000f33c
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2023-01-19 07:54:02 -0300

    fix: Button styles

--- frontend/src/components/Button/styles.js
+++ frontend/src/components/Button/styles.js
@@ -2,41 +2,48 @@ import styled, { css } from 'styled-components';
 
 export const StyledButton = styled.button`
   height: 52px;
-  border: none;
   padding: 0 16px;
-  background: ${({ theme }) => theme.colors.primary.main};
-  font-size: 16px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
-  font-weight: bold;
-  color: #fff;
-  border: 2px solid #fff;
+
+  border: 0;
   border-radius: 4px;
-  transition: background 0.2s ease-in;
+
+  background: ${({ theme }) => theme.colors.primary.main};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+
+  font-size: 16px;
+  font-weight: bold;
+  color: #FFF;
+
+  transition: all .2s ease-in;
   display: flex;
   align-items: center;
   justify-content: center;
 
-  &:hover{
-    background: 2px solid ${({ theme }) => theme.colors.primary.light}
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light};
+    transition: all .2s ease-in;
   }
 
-  &:active{
-    background: 2px solid ${({ theme }) => theme.colors.primary.dark}
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark};
+    transition: all .2s ease-in;
   }
 
-  &[disabled]{
-    background: #ccc !important;
-    cursor: default  !important;
+  &:disabled {
+    background: #CCC !important;
+    cursor: default !important;
   }
 
   ${({ theme, danger }) => danger && css`
     background: ${theme.colors.danger.main};
 
-    &:hover{
-    background: theme.colors.danger.light
+    &:hover {
+      background: ${theme.colors.danger.light};
     }
 
-    &:active{
-    background: theme.colors.danger.dark}
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
   `}
+
 `;
